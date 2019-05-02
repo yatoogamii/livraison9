e6314ba374475277bcb4daea7b09f6d614e6ae75
yatoogamii <bracaval.elias.pro@gmail.com>
2019-05-03 01:22:39 +0200

    change quote random for adep quotes, add quoteAdep.md for stock allQuotes

--- src/quotation/Quotation.js
+++ src/quotation/Quotation.js
@@ -19,7 +19,16 @@ import '../App.css';
 export default class Quotation extends React.Component {
 
   state = {
-    quotes: [], 
+    quotes: [
+      " On attend pas kenny ?", 
+      "C'est pas Vim way ça", 
+     "Bon weekend", 
+     "Répondez pas tous en même temps", 
+     "je vais peut être être papa", 
+     "On fait le pont vendredi ?", 
+     "Une p'tite bière ?"
+    ], 
+    author: ''
   };
 
   componentDidMount() {
@@ -33,19 +42,17 @@ export default class Quotation extends React.Component {
   getNewQuote() {
     axios.get(`https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`)
       .then(res => {
-        console.log(res);
         this.setState({
-          quotes: res.data.quotes[_.random(0, res.data.quotes.length -1)]
+          author: res.data.quotes[_.random(0, res.data.quotes.length -1)].author
         });
-        console.log(typeof(this.state.quotes));
       });
   }
 
   render() {
     return (
       <div className="Quotation">
-        <Quote quote={this.state.quotes.quote}></Quote>
-        <Author author={this.state.quotes.author}></Author>
+        <Quote quote={this.state.quotes[_.random(0, this.state.quotes.length -1)]}></Quote>
+        <Author author={this.state.author}></Author>
         <Btn onClick={() => this.nextQuote()}></Btn>
       </div>
     );
---
+++ src/quotation/quoteAdep.md
@@ -0,0 +1,9 @@
+# Quote Adep
+
+- On attend pas kenny ?
+- C'est pas Vim way ça
+- Bon weekend
+- Répondez pas tous en même temps
+- je vais peut être être papa
+- On fait le pont vendredi ?
+- Une p'tite bière ?
