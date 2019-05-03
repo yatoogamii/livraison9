ee5d7f0772357fdc3eeda54bc5743957c2c4c03e
yatoogamii <bracaval.elias.pro@gmail.com>
2019-05-03 21:19:54 +0200

    fix warnings, add alt img and remove useless import in Btn.js

--- src/Icon.js
+++ src/Icon.js
@@ -6,10 +6,6 @@ import React from 'react';
 
 import './App.css';
 
-// Material-ui
-
-import { withStyles } from '@material-ui/core/styles';
-
 // Variables 
 
 /////////////////////
@@ -19,10 +15,10 @@ export default class Icon extends React.Component {
     return (
       <div className="icons">
         <a href="https://www.linkedin.com/in/elias-bracaval-517678174/" target="no_blank">
-          <img src="https://img.icons8.com/carbon-copy/200/000000/linkedin.png" />
+          <img src="https://img.icons8.com/carbon-copy/200/000000/linkedin.png" alt="icon linkedin" />
         </a>
         <a href="https://twitter.com/BracavalE" target="no_blank">
-          <img src="https://img.icons8.com/carbon-copy/200/000000/twitter.png" />
+          <img src="https://img.icons8.com/carbon-copy/200/000000/twitter.png" alt="icon twitter" />
       </a>
       </div>
     );
